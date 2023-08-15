c0e6cf6af469249799a53875f011800130725192
kincaidkc <kincaidkc@ornl.gov>
2023-08-15 15:21:09 -0400

    Fix second instance of default value

--- applications/solvers/additiveFoam/BCs/marangoni/marangoniFvPatchVectorField.C
+++ applications/solvers/additiveFoam/BCs/marangoni/marangoniFvPatchVectorField.C
@@ -57,7 +57,7 @@ Foam::marangoniFvPatchVectorField::marangoniFvPatchVectorField
 :
     transformFvPatchField<vector>(p, iF),
     dSigmadT_(dict.lookup<scalar>("dSigmadT")),
-    Tmax_(dict.lookupOrDefault<scalar>("Tmax", 0.0))
+    Tmax_(dict.lookupOrDefault<scalar>("Tmax", GREAT))
 {
     evaluate();
 }
